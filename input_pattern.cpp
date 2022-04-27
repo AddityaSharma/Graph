a226445d3bb29c21646f3fa705956240385c34cd
Adi_Anant <72218536+AddityaSharma@users.noreply.github.com>
2022-04-27 22:08:35 +0530

    Update input_pattern.cpp

--- input_pattern.cpp
+++ input_pattern.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// adjacency matrix representation:
+// adjacency matrix representation -> undirected graph:
 int main() {
 	int n, m;
 	cin >> n >> m; 
@@ -10,7 +10,7 @@ int main() {
 	int adj[n+1][n+1]; 
 	
 	// take edges as input 
-	for(int i = 0;i<m;i++) {
+	for(int i = 0; i < m; i++) {
 	    int u, v; 
 	    cin >> u >> v;
 	    adj[u][v] = 1; 
@@ -19,7 +19,26 @@ int main() {
 	return 0;
 }
 
-// adjacency list representation:
+
+// adjacency matrix representation -> directed graph:
+int main() {
+	int n, m;
+	cin >> n >> m; 
+	
+	// declare the adjacent matrix 
+	int adj[n+1][n+1]; 
+	
+	// take edges as input 
+	for(int i = 0; i < m; i++) {
+	    int u, v; 
+	    cin >> u >> v;
+	    adj[u][v] = 1; 
+	}
+	return 0;
+}
+
+
+// adjacency list representation -> undirected graph:
 int main() {
 	int n, m;
 	cin >> n >> m; 
@@ -28,7 +47,7 @@ int main() {
 	vector<int> adj[n+1]; 
 	
 	// take edges as input 
-	for(int i = 0;i<m;i++) {
+	for(int i = 0; i < m; i++) {
 	    int u, v; 
 	    cin >> u >> v;
 	    adj[u].push_back(v); 
@@ -36,3 +55,56 @@ int main() {
 	}
 	return 0;
 }
+
+// adjacency list representation -> directed graph:
+int main() {
+	int n, m;
+	cin >> n >> m; 
+	
+	// declare the adjacent matrix 
+	vector<int> adj[n+1]; 
+	
+	// take edges as input 
+	for(int i = 0; i < m; i++) {
+	    int u, v; 
+	    cin >> u >> v;
+	    adj[u].push_back(v); 
+	}
+	return 0;
+}
+
+// adjacency list representation -> undirected graph with weights:
+int main() {
+	int n, m;
+	cin >> n >> m; 
+	
+	// declare the adjacent matrix 
+	vector<pair<int, int>> adj[n+1]; 
+	
+	// take edges as input 
+	for(int i = 0; i < m; i++) {
+	    int u, v, wt; 
+	    cin >> u >> v >> wt;
+	    adj[u].push_back({v, wt}); 
+	    adj[v].push_back({u, wt}); 
+	}
+	return 0;
+}
+
+// adjacency list representation -> undirected graph with weights:
+int main() {
+	int n, m;
+	cin >> n >> m; 
+	
+	// declare the adjacent matrix 
+	vector<pair<int, int>> adj[n+1]; 
+	
+	// take edges as input 
+	for(int i = 0; i < m; i++) {
+	    int u, v, wt; 
+	    cin >> u >> v >> wt;
+	    adj[u].push_back({v, wt}); 
+	}
+	return 0;
+}
+
