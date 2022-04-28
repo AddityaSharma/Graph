c569835442d090d0d87727eab13fc80ab5239f6e
Adi_Anant <72218536+AddityaSharma@users.noreply.github.com>
2022-04-28 11:48:12 +0530

    Create Traversals.cpp

---
+++ Traversals.cpp
@@ -0,0 +1,51 @@
+// BFS:
+vector<int> bfs(int V, vector<int> adj[]) {
+    vector<int> traversal;
+    
+    for(int i = 0; i < V; i++){
+        if(!visited[i]){
+            queue<int> q;
+            q.push(i);
+            visited[i] = 1;
+
+            while(!q.empty()){
+                int u = q.front();
+                q.pop();
+                traversal.push_back(u);
+                for(auto x : adj[u]){
+                    if(!visited[x]){
+                        visited[x] = 1;
+                        q.push(x);
+                    }
+                }
+            }
+        }  
+    }
+    
+    return traversal;
+}
+
+// DFS:
+void DFSrec(vector<int> adj[], int s, vector<bool> &visited, vector<int> &traversal){
+    visited[s] = 1;
+    traversal.push_back(s);
+    for(auto i : adj[s]){
+        if(!visited[i]){
+            DFSrec(adj, i, visited, traversal);
+        }
+    }
+}
+
+vector<int> dfs(int V, vector<int> adj[]){
+    vector<int> traversal;
+    vector<bool> visited(V+1, 0);
+    
+    // check nodes are 0-based or 1-based indexed.
+    for(int i = 1; i <= V; i++){ 
+        if(!visited[i]) DFSrec(adj, i, visited, traversal);
+    }
+    
+    return traversal;
+}
+
+
