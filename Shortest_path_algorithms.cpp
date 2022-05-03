6b8dde7d4f2ea7aabf4413169fff007cc3eb1369
Adi_Anant <72218536+AddityaSharma@users.noreply.github.com>
2022-05-03 13:54:38 +0530

    Update Shortest_path_algorithms.cpp

--- Shortest_path_algorithms.cpp
+++ Shortest_path_algorithms.cpp
@@ -69,3 +69,50 @@ void shortestPath(int src, int N, vector<pair<int,int>> adj[]){
     // print shortest distance for all nodes
 	for (int i = 0; i < N; i++) (dist[i] == 1e9)? cout << "INF ": cout << dist[i] << " "; 
 }
+
+// Shortest Path in Undirected Graph from a given source - Dijkstra's Algorithm:
+// In Dijkstra's Algorithm, we use a minimum priority queue to keep track of nodes instead of a simple queue data structure, we store nodes in <distance, node> 
+// fashion. initially we put the source node in the priority queue as <0, source_node>, where 0 is the distance of source node from itself. after this we 
+// implement a bfs kind of an algorithm. we maintain a distance array and mark the distance of all the nodes to be INT_MAX and marking the distance of source node
+// to be zero, then in each iteration we take out a node from the priority queue and traverse all its adjacent nodes, while traversing the nodes, we compute the 
+// distance required to reach that node and if the newly computer distance is lesser than the one already there in the distance array, then we update the shorter
+// distance in the distance array and at the same time put that node along with the shorter distance in the priority queue. we keep repeating the same process, until 
+// the priority queue becomes empty.
+// now there can be instances where we have multiple instances of a single node in our priority queue, and thats totally fine, as we are placing the elements in
+// priority queue as <distance, node> the occurance with minimum distance will be always on the top and we will always perfrom our logic using that node first and 
+// then will all the other occurances of that node, this way they are simply ignored as they will never provide us with a shorter distance. 
+
+// algorithm step-by-step explanation:
+// 1) We would be using a min-heap and a distance array of size N initialized with infinity (indicating that at present none of the nodes are reachable from the
+// source node) and initialize the distance to source node as 0.
+// 2) We push the source node to the queue.
+// 3) For every node at the top of the queue we pop that element out and look out for its adjacent nodes. If the current reachable distance is better than the 
+// previous distance indicated by the distance array, we update the distance and push it in the queue.
+// 4) A node with a lower distance would be at the top of the priority queue as opposed to a node with a higher distance. By following the steps 3, until our queue
+// becomes empty, we would get the minimum distance from the source node to all other nodes.
+
+void dijkstra_Algorithm(vector<pair<int, int>> adj[], int N, int src){
+	priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int>>> pq; // minimum priority queue
+    vector<int> distTo(V+1, INT_MAX); // 0-indexed array for calculating shortest paths
+    	
+    distTo[src] = 0;
+    pq.push(make_pair(0,src));	// <distance ,source>
+    	
+    while(!pq.empty()){
+    	int curr_node_dist = pq.top().first;
+    	int curr_node = pq.top().second;
+    	pq.pop();
+    
+    	for(auto it : adj[curr_node]){
+    		int next_node = it->first;
+    		int next_node_dist = it->second;
+    		if(distTo[next_node] > distTo[curr_node] + next_node_dist){
+    			distTo[next_node] = distTo[curr_node] + next_node_dist;
+    			pq.push(make_pair(distTo[next_node], next_node));
+    		}
+    	}
+    }
+	
+	cout << "The distances from source " << src << " are : \n";
+	for(int i = 1 ; i<=n ; i++)	cout << distTo[i] << " ";
+}
