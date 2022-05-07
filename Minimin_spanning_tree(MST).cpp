457f5b67c0ebe224c246d3a742a6dad167d34fde
Adi_Anant <72218536+AddityaSharma@users.noreply.github.com>
2022-05-07 12:52:37 +0530

    Update Minimin_spanning_tree(MST).cpp

--- Minimin_spanning_tree(MST).cpp
+++ Minimin_spanning_tree(MST).cpp
@@ -35,7 +35,7 @@ void unionn(int u, int v, vector<int> &parent, vector<int> &rank) {
 // Disjoint set implementations ends.
 
 int main(){
-  // taking a graph as input directly in a vector named 'edges'.
+    // taking a graph as input directly in a vector named 'edges'.
 	int N=5,m=6;
 	vector<node> edges; 
 	edges.push_back(node(0,1,2));
@@ -80,34 +80,77 @@ int main(){
 
 // Prim's Algorithm Implementation:
 
+// use this implementation if we need the edges that are required in the MST.
 void prims_Algorithm(vector<pair<int, int> adj[], int N){
-  vector<int> parent(N, -1);
-  vector<int> key(N, INT_MAX);
-  vector<int> mstSet(N, 0);
-  priority_queue<pair<int,int>, vector <pair<int,int>> , greater<pair<int,int>>> pq; // pq format - <weight, node>
+	vector<int> parent(N, -1);
+ 	vector<int> key(N, INT_MAX);
+  	vector<int> mstSet(N, 0);
+  	priority_queue<pair<int,int>, vector <pair<int,int>> , greater<pair<int,int>>> pq; // pq format - <weight, node>
   
-  key[0] = 0; 
-  parent[0] = -1; 
-  pq.push({0, 0});
+  	key[0] = 0; 
+  	parent[0] = -1; 
+  	pq.push({0, 0});
 
-  while(!pq.empty()){ 
-      int u = pq.top().second; // every node which has been into priority queue once, will become part of MST as soon as it is taken out from pq. 
-      pq.pop(); 
-      mstSet[u] = true; 
+  	while(!pq.empty()){ 
+      	int u = pq.top().second; // every node which has been into priority queue once, will become part of MST as soon as it is taken out from pq. 
+      	pq.pop(); 
+      	mstSet[u] = true; 
 
-      for (auto it : adj[u]) {
-          int v = it.first;
-          int weight = it.second;
-          if (!mstSet[v] && weight < key[v]){ // make sure while traversing the adjacent nodes, we will not take in account the nodes which are already in the MST.
-              parent[v] = u;
-              key[v] = weight; 
-              pq.push({weight, v}); // when we put a node in our pq, we mean to say that we have reached out that node via the minimum possible distance so far.
-              // also having multiple instances of a single node with different weights in our priority queue is totally fine, as for a given node we always take the
-              // one with the minimum weight and use it to traverse its adjacency list, this way when we again encounter that node, we simply ignore them.
-          }
-      }
-  } 
+      	for(auto it : adj[u]) {
+          	int v = it.first;
+          	int weight = it.second;
+          	if (!mstSet[v] && weight < key[v]){ // make sure while traversing the adjacent nodes, we will not take in account the nodes which are already in the MST.
+              	parent[v] = u;
+              	key[v] = weight; 
+              	pq.push({weight, v}); // when we put a node in our pq, we mean to say that we have reached out that node via the minimum possible distance so far.
+              	// also having multiple instances of a single node with different weights in our priority queue is totally fine, as for a given node we always take the
+              	// one with the minimum weight and use it to traverse its adjacency list, this way when we again encounter that node, we simply ignore them.
+          	}
+      	}
+  	} 
 
-  // printing out the nodes of the MST.
-  for (int i = 1; i < N; i++) cout << parent[i] << " - " << i << " \n";
+  	// printing out the nodes of the MST.
+  	for (int i = 1; i < N; i++) cout << parent[i] << " - " << i << " \n";
+}
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// if we want the cost of the edges:
+// --> one way is to traverse the edges we have marked that will be in our MST.
+// --> another way is to add the cost of the edges when we mark an edge to be in the MST. - (code below)
+int prims_algorithm(int V, vector<vector<int>> adj[]){
+	vector<int> parent(V, -1);
+    vector<int> key(V, INT_MAX);
+    vector<int> mstSet(V, 0);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    
+    key[0] = 0;
+    parent[0] = -1;
+    mstSet[0] = 1;
+    pq.push({0, 0}); // -> {weight, node}
+    
+    int cost = 0;
+    
+    while(!pq.empty()){
+        int u = pq.top().second;
+        int edgeWeight = pq.top().first;
+        pq.pop();
+        
+        if(!mstSet[u]){ // this is the only change we have made to get the cost of the MST.
+            mstSet[u] = 1;
+            cost += edgeWeight;
+        }
+        
+        for(auto v : adj[u]){
+            int node = v[0];
+            int weight = v[1];
+            if(!mstSet[node] && weight < key[node]){
+                key[node] = weight;
+                parent[node] = u;
+                pq.push({weight, node});
+            }
+        } 
+    }
+    
+    return cost;
 }
